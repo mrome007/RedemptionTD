abc248b2fd826841ec545b690fa1021618bc9b12
mrome007 <mrome007@ucr.edu>
2018-09-09 18:00:51 -0700

    Add simple enemies, base, and weapons. Make the game look blurry and use gameboy color schemes.

---
+++ Assets/Scripts/DummyMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyMove : MonoBehaviour 
+{
+	[SerializeField]
+	private float speed;
+	
+	// Update is called once per frame
+	private void Update () 
+	{
+		transform.Translate(Vector2.right * speed * Time.deltaTime);
+	}
+}
