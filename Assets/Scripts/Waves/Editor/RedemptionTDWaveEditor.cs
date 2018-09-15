6ad6a38d32f87a81eec2f2c192aa77919f915bbc
mrome007 <mrome007@ucr.edu>
2018-09-15 12:26:33 -0700

    Set up classes needed for redemption td wave system. Will be creating a custom editor for waves.

---
+++ Assets/Scripts/Waves/Editor/RedemptionTDWaveEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RedemptionTDWave))]
+public class RedemptionTDWaveEditor : Editor
+{
+    private RedemptionTDWave redemptionWave;
+
+    private void OnEnable()
+    {
+        redemptionWave = target as RedemptionTDWave;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if(redemptionWave == null)
+        {
+            redemptionWave = target as RedemptionTDWave;
+        }
+
+        DrawAddSpawnButton();
+    }
+
+    private void DrawAddSpawnButton()
+    {
+        if(GUILayout.Button("Add Wave", EditorStyles.miniButton))
+        {
+            AddWave();
+        }
+    }
+
+    private void AddWave()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Waves/RedemptionTDSpawnInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedemptionTDSpawnInfo : MonoBehaviour 
+{
+    [HideInInspector]
+    public float StartSpawnDelay;
+
+    [HideInInspector]
+    public float StopSpawnDelay;
+
+    [HideInInspector]
+    public float TimeBetweenSpawns;
+
+    [HideInInspector]
+    public RedemptionEnemyType EnemyType;
+
+    [HideInInspector]
+    public bool IsBoss;
+
+    [HideInInspector]
+    public Transform SpawnPosition;
+}
