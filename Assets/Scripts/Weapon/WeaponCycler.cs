9573f44014ae62449f2f25ecd462114cd30c6a8c
mrome007 <mrome007@ucr.edu>
2018-10-16 21:11:43 -0700

    Add an extra object in wave mode that is primarily a collider that acts as a trigger range to activate weapon behavior. Also add weapon cycler.

---
+++ Assets/Scripts/Weapon/WeaponCycler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCycler : MonoBehaviour 
+{
+    [SerializeField]
+    private List<GameObject> weapons;
+    
+    private int currentWeaponIndex = 0;
+
+    private void Start()
+    {
+        weapons.ForEach(weapon => weapon.SetActive(false));
+        weapons[currentWeaponIndex].SetActive(true);
+    }
+}
