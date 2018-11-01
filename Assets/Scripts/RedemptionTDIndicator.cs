452785f1a5410008c1b39fc31baaec6513168517
mrome007 <mrome007@ucr.edu>
2018-10-31 19:57:52 -0700

    Create indicator to show on screen where the general direction of the enemies off screen.

---
+++ Assets/Scripts/RedemptionTDIndicator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedemptionTDIndicator : MonoBehaviour 
+{
+    private Vector3 originalPosition;
+
+    private float offset = 0.1f;
+
+    private void Awake()
+    {
+        originalPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if(InCameraView())
+        {
+            transform.position = originalPosition;
+            return;
+        }
+
+        var bottomLeftCam = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, Camera.main.nearClipPlane));
+        var topRightCam = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, Camera.main.nearClipPlane));
+
+        var xPos = Mathf.Clamp(originalPosition.x, bottomLeftCam.x + offset, topRightCam.x - offset);
+        var yPos = Mathf.Clamp(originalPosition.y, bottomLeftCam.y + offset, topRightCam.y - offset);
+
+        transform.position = new Vector3(xPos, yPos, -1f);
+    }
+
+    private bool InCameraView()
+    {
+        var bottomLeftCam = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, Camera.main.nearClipPlane));
+        var topRightCam = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, Camera.main.nearClipPlane));
+        return originalPosition.x > bottomLeftCam.x + offset && originalPosition.x < topRightCam.x - offset
+            && originalPosition.y > bottomLeftCam.y + offset && originalPosition.y < topRightCam.y - offset;
+    }
+}
