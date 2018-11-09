d680768c3e4b08d95255319334ab76553976d855
mrome007 <mrome007@ucr.edu>
2018-11-08 19:08:54 -0800

    Create timer. This will be used for the rating system after a level is finished.

---
+++ Assets/RedemptionTDTimer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RedemptionTDTimer : MonoBehaviour 
+{
+    public float CurrentTime { get { return currentTime; } }
+    private float currentTime;
+    private bool timerStarted;
+    private StringBuilder timerBuffer;
+    private const string timerFormat = "{0}:{1}:{2}";
+    [SerializeField]
+    private Text timerText;
+
+    private void Awake()
+    {
+        timerBuffer = new StringBuilder();
+    }
+
+    private void Start()
+    {
+        StartTimer();
+    }
+
+    public void StartTimer()
+    {
+        timerStarted = true;
+        currentTime = 0f;
+    }
+
+    public void StopTimer()
+    {
+        timerStarted = false;
+    }
+
+    private void Update()
+    {
+        if(!timerStarted)
+        {
+            return;
+        }
+
+        timerBuffer.Length = 0;
+        currentTime += Time.deltaTime;
+        timerBuffer.AppendFormat(timerFormat, Mathf.Floor(currentTime / 60f).ToString("00"), 
+                                (Mathf.Floor(currentTime) % 60).ToString("00"), 
+                                ((currentTime % 1f) * 100f).ToString("00"));
+        timerText.text = timerBuffer.ToString();
+    }
+}
